103a963e4370e269de7cd4001f141b85f4d65c69
SamanthaVelasquez4 <samvel200355@gmail.com>
2025-07-09 23:56:41 -0600

    Prueba con git

--- scripts/generador_compras.py
+++ scripts/generador_compras.py
@@ -53,3 +53,6 @@ if __name__ == "__main__":
 
     # Ejecutar el script Bash que procesa el CSV y genera la factura PDF
     subprocess.run(["bash", "./scripts/generador_facturas.sh"])
+
+    #Samantha estuvo aqui
+    
